e20298e31cd157ed2d4d8b25c1f9a644e9969942
David <david@kidsnote.com>
2017-03-01 23:32:32 +0900

    all urls에 api 추가

--- base/conf/urls/all.py
+++ base/conf/urls/all.py
@@ -22,4 +22,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^shortener/', include('shortener.urls', namespace='shortener')),
+    url(r'^api/', include('api.urls', namespace='api')),
 ]
