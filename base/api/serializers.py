cf8d9e96a6e26d78caa86280bee365dbf9f1c504
David <david@kidsnote.com>
2017-03-06 00:05:56 +0900

    serializers 변경

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from shortener.models import Shortener
 
 
-class ShortenerSerializers(serializers.HyperlinkedModelSerializer):
+class ShortenerSerializers(ModelSerializer):
+
     class Meta:
         model = Shortener
         fields = ('short_url', 'link_url', 'status')
